8bdad74f26b6238f49d724561111e5a89133d25a
Eliott Wantz <dev.eliottdev@gmail.com>
2022-08-26 10:50:29 -0400

    feat(server):

--- server/handlers/continent.go
+++ server/handlers/continent.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"app/db"
 	"app/models"
+	"fmt"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,4 +13,14 @@ func GetAllContinents(c *fiber.Ctx) error {
 	var continents []models.Continent
 	db.DB.Find(&continents)
 	return c.JSON(continents)
+}
+
+func GetContinent(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return fmt.Errorf("%v : %v", fiber.ErrBadRequest, err)
+	}
+	var continent models.Continent
+	db.DB.First(&continent, id)
+	return c.JSON(continent)
 }
